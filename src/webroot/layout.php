c45c33e35dd716787dfffbda440ba5a8bec6d06d
George Dicu <dicu.george007@gmail.com>
2012-09-06 03:00:24 +0300

    basic layout implementation

---
+++ src/webroot/layout.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+<link rel="stylesheet" type="text/css" href="style.css" />
+
+<title>Here goes the title</title>
+
+</head>
+<body>
+<div id="wrapper">
+
+    <div id="picture">
+        <p>Picture</p>
+    </div>
+
+    <div id="header">
+        here goes menu links and seach bar
+    </div>
+
+    <div id="user-area">
+        here goes user area
+    </div>
+</div>
+
+<div id="books-wrapper">
+    here goes books info
+</div>
+
+<div id="footer">
+    here goes copyright info
+</div>
+<body>
+</html>
