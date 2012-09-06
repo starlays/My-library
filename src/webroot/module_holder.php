c24b626db76499cf9b2f1dd37ae426139bfaf543
Lazar Florentin <florentin.lazar@gmail.com>
2012-09-06 23:29:51 +0300

    add module holder file

---
+++ src/webroot/module_holder.php
@@ -0,0 +1,9 @@
+// all the available pages are stored here
+
+return array (
+    'home' => array (
+        'title'     => 'Home page',
+        'content'   => 'home.php'
+    )
+// add here values
+);
