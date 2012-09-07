7b1a650ada0b7d463a62090bc75707991683e66b
Lazar Florentin <florentin.lazar@gmail.com>
2012-09-07 09:15:07 +0300

    add pages to array

--- src/webroot/module_holder.php
+++ src/webroot/module_holder.php
@@ -1,9 +1,20 @@
 // all the available pages are stored here
 
 return array (
-    'home' => array (
+    'home'  => array (
         'title'     => 'Home page',
-        'content'   => 'home.php'
+        'content'   => 'base.php'
+    ),
+    'admin' => array (
+        'title'     => 'Admin page',
+        'content'   => 'admin.php'
+    ),
+    'books' => array (
+        'title'     => 'Books page',
+        'content'   => 'books.php'
+    ),
+    'user'  => array (
+        'title'     => 'User page',
+        'content'   => 'user.php'
     )
-// add here values
 );
