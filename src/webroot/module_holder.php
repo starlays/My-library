fcfcdb44d0e75339f1c232286dc2107df86ed602
Lazar Florentin <florentin.lazar@gmail.com>
2012-09-07 16:12:41 +0300

    add pages and the required configuration for each page

--- src/webroot/module_holder.php
+++ src/webroot/module_holder.php
@@ -1,20 +1,25 @@
+<?php
 // all the available pages are stored here
 
 return array (
     'home'  => array (
         'title'     => 'Home page',
-        'content'   => 'base.php'
+        'in_menu'   => TRUE,
+        'content'   => 'BASE_FUNCTION.php'
     ),
     'admin' => array (
         'title'     => 'Admin page',
+        'in_menu'   => TRUE,
         'content'   => 'admin.php'
     ),
     'books' => array (
         'title'     => 'Books page',
+        'in_menu'   => TRUE,
         'content'   => 'books.php'
     ),
     'user'  => array (
         'title'     => 'User page',
+        'in_menu'   => TRUE,
         'content'   => 'user.php'
     )
 );
