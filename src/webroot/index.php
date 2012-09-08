ae6b9efaba5f8152d89d6fe4a18de17df06f40f0
Lazar Florentin <florentin.lazar@gmail.com>
2012-09-08 12:03:48 +0300

    - fix dublicate cod - replaced functions name in path with modules

--- src/webroot/index.php
+++ src/webroot/index.php
@@ -3,8 +3,10 @@
 define('__APPROOT__', dirname(dirname(__FILE__)).DIRECTORY_SEPARATOR);
 //define root
 define('__ROOT__', dirname(__FILE__).DIRECTORY_SEPARATOR);
+//modules path
+define('__MODULES__', __APPROOT__.DIRECTORY_SEPARATOR.'modules'.DIRECTORY_SEPARATOR);
 //base app functions file
-$base_fns_file  = __APPROOT__.'functions'.DIRECTORY_SEPARATOR.'base'
+$base_fns_file  = __APPROOT__.'modules'.DIRECTORY_SEPARATOR.'base'
                                         .DIRECTORY_SEPARATOR.'functions.php';
 //modules holder
 $pages_fl = __ROOT__.'module_holder.php';
@@ -16,23 +18,15 @@ $errors = array();
 //base error constants
 const ERR_BASEFN = 10;
 
-// TODO: Dublicated code, write a function{
-if(file_exists($base_fns_file) && is_readable($base_fns_file)) {
-    require_once ($base_fns_file);
+if(file_exists($base_fns_file) && is_readable($base_fns_file)
+             && file_exists($pages_fl) && is_readable($pages_fl)) {
+     require_once ($base_fns_file);
+     $modules = require_once ($pages_fl);
 }
 else {
     echo sprintf('Error: %d', ERR_BASEFN);
     exit();
 }
-
-if(file_exists($pages_fl) && is_readable($pages_fl)) {
-    $modules = require_once ($pages_fl);
-}
-else {
-    echo sprintf('Error: %d', ERR_BASEFN);
-    exit();
-}
-// }
 
 if(isset($_GET['page'])) {
     $required_page = htmlspecialchars($_GET['page']);
@@ -47,7 +41,6 @@ if(isset($_GET['page'])) {
 else {
     $page = 'home';
 }
-
 
 //variable holding all the vars that will go from BL to VL trough render
 //function
