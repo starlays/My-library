0742b9126beb5c2781c595aef036604d934de6e3
George Dicu <dicugeorge1987@yahoo.com>
2012-09-08 15:49:52 +0300

    changing in_menu metadata value to have more possible separated menus

--- src/webroot/module_holder.php
+++ src/webroot/module_holder.php
@@ -4,27 +4,41 @@
 return array (
     'home'  => array (
         'title'     => 'Home page',
-        'in_menu'   => TRUE,
+        'in_menu'   => 1,
         'content'   => 'home.php'
     ),
     'books' => array (
         'title'     => 'Books page',
-        'in_menu'   => TRUE,
+        'in_menu'   => 1,
         'content'   => 'books.php'
     ),
     'user'  => array (
         'title'     => 'User page',
-        'in_menu'   => TRUE,
+        'in_menu'   => 1,
         'content'   => 'user.php'
     ),
     'admin' => array (
         'title'     => 'Admin page',
-        'in_menu'   => TRUE,
+        'in_menu'   => 1,
         'content'   => 'admin.php'
     ),
     'mysql' => array (
-        'in_menu'   => FALSE,
-        'content'   => 'admin.php'
+        'in_menu'   => 0,
+        'content'   => 'mysql.php'
+    ),
+	 'register' => array (
+	    'title'     => 'Register',
+        'in_menu'   => 2,
+        'content'   => 'register.php'
+    ),
+	 'login' => array (
+	    'title'     => 'Login',
+        'in_menu'   => 2,
+        'content'   => 'login.php'
+    ),
+	 'recover' => array (
+	    'title'     => 'Recover',
+        'in_menu'   => 2,
+        'content'   => 'recover.php'
     )
-
 );
