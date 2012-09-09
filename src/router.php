6d9e157506f141642be9a97f05d908705092614c
Lazar Florentin <florentin.lazar@gmail.com>
2012-09-09 13:10:08 +0300

    fix #1

--- src/router.php
+++ src/router.php
@@ -1,15 +1,13 @@
 <?php
+
+$page = 'home';
+
 if(isset($_GET['page'])) {
     $required_page = htmlspecialchars($_GET['page']);
 
     if(isset($modules[$required_page]) && $modules[$required_page]['in_menu']) {
         $page = $required_page;
     }
-    else {
-        $page = 'home';
-    }
 }
-else {
-    $page = 'home';
-}
+
 return $page;
