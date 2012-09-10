2db89f7e1aa171424bb629ddba84dedc2d57f139
Lazar Florentin <florentin.lazar@gmail.com>
2012-09-10 20:31:00 +0300

    add books VL file

---
+++ src/modules/books/books.php
@@ -0,0 +1,14 @@
+<div id="books">
+    <div id="book_tile">
+        here be book title
+    </div>
+    <div id="book_author">
+        here be author
+    </div>
+    <div id="book_description">
+        here be book description
+    </div>
+    <div id="book_add_date">
+        here be add date
+    </div>
+</div>
