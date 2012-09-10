a4380cca96fbf49a60d62031b3c86f37ed221e2c
George Dicu <dicu.george007@gmail.com>
2012-09-10 03:49:59 +0300

    double slash bug fix! in line 7(defineing modules absolute patch)

--- src/webroot/index.php
+++ src/webroot/index.php
@@ -4,7 +4,7 @@ define('__ROOT__', dirname(__FILE__).DIRECTORY_SEPARATOR);
 //define app root
 define('__APPROOT__', dirname(__ROOT__).DIRECTORY_SEPARATOR);
 //modules path
-define('__MODULES__', __APPROOT__.DIRECTORY_SEPARATOR.'modules'.DIRECTORY_SEPARATOR);
+define('__MODULES__', __APPROOT__.'modules'.DIRECTORY_SEPARATOR);
 //base app functions file
 $base_fns_file  = __APPROOT__.'modules'.DIRECTORY_SEPARATOR.'base'
                                         .DIRECTORY_SEPARATOR.'functions.php';
