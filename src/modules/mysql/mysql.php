2281a2a21b88dd6832b25e4230d7b39853ba4874
Lazar Florentin <florentin.lazar@gmail.com>
2012-09-10 23:25:31 +0300

    renamed var from $table to $DB

--- src/modules/mysql/mysql.php
+++ src/modules/mysql/mysql.php
@@ -2,11 +2,11 @@
 $mysql_host        =    ''; //MySQL host
 $mysql_user        =    ''; //MySQL user
 $mysql_pass        =    ''; //MySQL pass
-$table              =    'mylibrary';
+$DB                =    'mylibrary'; //MySQL database
 
-$mysql_link = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $table);
+$mysql_link = mysqli_connect($mysql_host, $mysql_user, $mysql_pass, $DB);
 
-unset($mysql_host, $mysql_user, $mysql_pass, $table);
+unset($mysql_host, $mysql_user, $mysql_pass, $DB);
 
 if($mysql_link) {
     return $mysql_link;
