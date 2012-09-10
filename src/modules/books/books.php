53cb1bbcb848cde6faba8d6add39ed92106fadeb
Lazar Florentin <florentin.lazar@gmail.com>
2012-09-10 23:19:14 +0300

    VL file outputs the vars recived from module BL file

--- src/modules/books/books.php
+++ src/modules/books/books.php
@@ -1,14 +1,12 @@
 <div id="books">
-    <div id="book_tile">
-        here be book title
-    </div>
-    <div id="book_author">
-        here be author
-    </div>
-    <div id="book_description">
-        here be book description
-    </div>
-    <div id="book_add_date">
-        here be add date
-    </div>
+<?php
+foreach($page_vl_vars as $metadata => $data) {
+    echo <<<EOT
+        <div id="$metadata">
+            $metadata : $data
+        </div>
+EOT;
+    echo PHP_EOL;
+}
+?>
 </div>
