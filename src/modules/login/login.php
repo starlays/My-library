1063137b4f491a3855d6f88a74df2f0c7134344d
George Dicu <dicu.george007@gmail.com>
2012-09-10 22:42:16 +0300

    added simple login form

---
+++ src/modules/login/login.php
@@ -0,0 +1,14 @@
+<form action='' method='POST'>
+
+<fieldset>
+<legend> Login : </legend>
+<label for='usr'>Username:</label><input id='usr' name='usr' type='text' /><br />
+<label for='pwd'>Password:</label><input id='pwd' name='pwd' type='password' /><br />
+</fieldset>
+<input type='submit' name='login' value='Login' />
+
+</form>
+<?php 
+//TODO: security, sanitize, auto require dependencies!
+require_once __MODULES__.$page.DIRECTORY_SEPARATOR.'login_BL.php';
+?>
