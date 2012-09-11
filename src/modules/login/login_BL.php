48a70da694a86f4080b5dff3897dfa1a8bf8ab4b
George Dicu <dicu.george007@gmail.com>
2012-09-11 17:25:29 +0300

    added login_BL code

--- src/modules/login/login_BL.php
+++ src/modules/login/login_BL.php
@@ -1,9 +1,22 @@
 <?php
-//TODO: security, sanitize, auto require dependencies!
+//TODO: security, sanitize
 //TODO: use only one retrun and a error holder!
+
 if(isset($_POST['login'])){
 	$reginfo = array($_POST['usr'], $_POST['pwd']);
 	list($usr,$pwd) = $reginfo;
-
+	
+	$query = "SELECT * FROM users WHERE username='$usr' AND password='$pwd'";
+	$qresult = mysqli_query($mysql_link,$query);
+	$ckuser = mysqli_num_rows($qresult);
+	
+	if(0 < $ckuser){
+		session_start();
+		$_SESSION['is_logged_in'] = 1;
+	}
+	else {
+		echo 'userul nu exista';
+	}
 }
+mysqli_close($mysql_link);
 
