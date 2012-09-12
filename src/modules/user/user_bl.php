9ccadaefe28e3bbd5ce5f598980c197a3916de88
Lazar Florentin <florentin.lazar@gmail.com>
2012-09-12 13:12:02 +0300

    finished module user add book

--- src/modules/user/user_bl.php
+++ src/modules/user/user_bl.php
@@ -1,33 +1,69 @@
 <?php
 //error const
 const USER_ADBOOK_EYFLD = 40;
+const USER_NODB_INSERT  = 41;
+const USER_DB_INSERT    = 42;
 
 //add book business logic
 if(isset($_POST['usr_add_book'])) {
-
-    if(!empty($_POST['book_title']) && !empty($_POST['book_author']) 
-            && !empty($_POST['book_descript']) && !empty($_POST['book_insdate'])) {
-        
+    $required_info = array($_POST['book_title'], $_POST['book_author'], 
+        $_POST['book_descript'], $_POST['book_insdate']);
+    $optional_info = array($_POST['book_cvrimg'], $_POST['book_ebook']);
+//TODO: Better form validation, check every case, check if field type is coresponding    
+    if(isEmpty($required_info)) {
         if($mysql_link){
-            $author = mysqli_real_escape_string($mysql_link, $_POST['book_author']);
-            $sql_author = "INSERT INTO `authors` (`name`) VALUES ('$author');";
-            
-            $book_title    = mysqli_real_escape_string($mysql_link, $_POST['book_title']);
-            $book_descript = mysqli_real_escape_string($mysql_link, $_POST['book_descript']);
-            $book_insdate  = mysqli_real_escape_string($mysql_link, $_POST['book_insdate']);
- 
-$sql = "INSERT INTO `mylibrary`.`books` 
-    (`id`, `title`, `id_author`, `description`, `insert_date`, `cvr_img_path`, `e_book_path`, `id_rate`, `id_insert_user`)
-    VALUES 
-    (NULL, \'test\', \'3\', \'test\', \'2012-09-12\', \'\', \'\', 2, 1)";
+        $required_info = datafilter($required_info);
+        $optional_info = datafilter($optional_info);
+        $required_info[] = $optional_info;
+        
+        //TODO: Use prepared statements instead of #18-#25
+        foreach($required_info as $data){
+            if(is_array($data)) {
+                foreach($data as $inner_data){
+                    mysqli_real_escape_string($mysql_link, $inner_data);
+                }
+            }
+            else {
+                mysqli_real_escape_string($mysql_link, $data);
+            }
+        }
 
-            $sql_book = "INSERT INTO `books` (``)";
-            
-            //use MySQL transactions for multiple insert
+        list($book_title, $book_author, $book_descript, $book_insdate,
+                list($book_cvrimg, $book_ebook)) = $required_info;
+        //optional informatin default value processing
+        $book_cvrimg = empty($book_cvrimg) ? NULL : $book_cvrimg;
+        $book_ebook = empty($book_ebook) ? NULL : $book_ebook;
+        
+        $sql_author   = "INSERT INTO `authors` (`name`) VALUES ('$book_author');";
+        $sql_add_book = "
+           INSERT INTO `books` (`title`, `id_author`, `description`, `insert_date`,
+           `cvr_img_path`, `e_book_path`, `id_rate`, `id_insert_user`)
+           VALUES ( '$book_title', (SELECT `id` FROM `authors` WHERE name='$book_author'), 
+           '$book_descript', '$book_insdate', '$book_cvrimg', '$book_ebook', 1, 2);"; 
+            //TODO: fix use id, now is inserted manualy
+          
+            //use MySQL transactions to be on the safe side
             mysqli_autocommit($mysql_link, FALSE);
-            mysqli_query($mysql_link, $sql_author);
-            mysqli_commit($mysql_link);
+            $insert_error = FALSE;
             
+            if(!mysqli_query($mysql_link, $sql_author)) {
+                $insert_error = TRUE;
+            }
+            if(!mysqli_query($mysql_link, $sql_add_book)) {
+                $insert_error = TRUE;
+            }
+            if($insert_error) {
+                mysqli_rollback($mysql_link);
+                mysqli_close($mysql_link);
+                
+                return USER_NODB_INSERT;
+            }
+            else {
+                mysqli_commit($mysql_link);
+                mysqli_close($mysql_link);
+                
+                return USER_DB_INSERT;
+            }
         }
     }
     else {
