d2984506daa60c54a9b3e51631497d4164526aa4
George Dicu <dicu.george007@gmail.com>
2012-09-12 21:00:06 +0300

    if the user is not logged in the logout button with redirect to login page

--- src/modules/session/session_destroy.php
+++ src/modules/session/session_destroy.php
@@ -1,7 +1,10 @@
 <?php
-session_start();
-//remove all the variables in the session 
+session_start(); 
 if (isset($_SESSION['is_logged_in'])){
+    //remove all the variables in the session 
+    session_unset(); 
+    // destroy the session 
+    session_destroy(); 
     header('Location: index.php');
 }
 else {
