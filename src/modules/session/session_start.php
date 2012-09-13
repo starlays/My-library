62a4ff5294d90ae206fd302288a5b5ef20873eef
George Dicu <dicu.george007@gmail.com>
2012-09-13 04:17:12 +0300

    this file is only starting a session if !present, doesnt redirect if logged in

--- src/modules/session/session_start.php
+++ src/modules/session/session_start.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
-if (isset($_SESSION['is_logged_in']) && $_SESSION['is_logged_in'] === 1) {
-    header('Location: index.php');
+if(!isset($_SESSION)){
+    session_start();
 }
