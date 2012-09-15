7b7c74d7d10ddb9a0c5ad38ed96ec84cd194b43a
Lazar Florentin <florentin.lazar@gmail.com>
2012-09-16 00:21:21 +0300

    fix resources_loader

--- src/resources_loader.php
+++ src/resources_loader.php
@@ -4,12 +4,12 @@ $resourcess_to_load = array();
 
 if(isset($modules[$page]['resources'])) {
 
-    foreach($modules[$page]['resources'] as $resources) {
-        $res_path = __MODULES__.$resources.DIRECTORY_SEPARATOR;
-
-        if(isset($modules[$resources]['resourcess']) &&
-            check_file($modules[$resources]['resources_file'], $res_path)) {
-                $resourcess_to_load[] =  $res_path.$modules[$resources]['resources_file'];
+    foreach($modules[$page]['resources'] as $resource) {
+        $res_path = __RESOURCES__.$resource.DIRECTORY_SEPARATOR;
+ 
+        if(isset($resources[$resource]['resource_file']) &&
+            check_file($$resources[$resource]['resource_file'], $res_path)) {
+                $resourcess_to_load[] =  $res_path.$resources[$resource]['resource_file'];
         }
     }
 }
