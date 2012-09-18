787e8b7e89c7f28f3be37596ff651e4a4c5f5d83
George Dicu <dicu.george007@gmail.com>
2012-09-18 23:30:27 +0300

    added new resources

--- src/resource_holder.php
+++ src/resource_holder.php
@@ -3,6 +3,12 @@
 
 return array (
     'mysql' => array (
-        'resource_file' => 'mysql.php'
-    )
+        'resource_file' => 'mysql.php',
+    ),
+    'login' => array (
+        'resource_file' => 'login_bl.php',
+    ),
+    'autologin' => array (
+        'resource_file' => 'autologin.php',
+    ),
 );
