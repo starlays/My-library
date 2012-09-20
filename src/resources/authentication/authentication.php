07812c7287f0226f0228b1154c2bddcf5a3a0818
Lazar Florentin <florentin.lazar@gmail.com>
2012-09-20 23:08:12 +0300

    add authentication resources provider

---
+++ src/resources/authentication/authentication.php
@@ -0,0 +1,123 @@
+<?php
+/**
+ * Authentication resource
+ */
+
+/**
+ * Resource error constants:
+ */
+
+/**
+ * Check to se if the given user exists in dadabase
+ * user_exists($mysql_link, $username,[$password])
+ * 
+ * @param resource $mysql_link an resource object link to the database
+ * @param string $username the user that you wish to check if exists
+ * @param string $password optional param the password attached to the $username
+ * 
+ * @return bool TRUE if user exists and otherwise FALSE
+ */
+function user_exists($mysql_link, $username=NULL) {
+    
+    $SQL = "SELECT username FROM `users` WHERE username='$username'";
+    
+    if(func_num_args()===3) {
+        if(is_string(func_get_arg(3))) {
+            $SQL.= " AND password='$pass'";
+        }
+    }
+    $qresult = mysqli_query($mysql_link, $SQL);
+    if($result  = mysqli_num_rows($qresult)) {
+        return TRUE;
+    }
+    else {
+        return FALSE;
+    }
+}
+
+/**
+ * Insert new registered user in to database
+ *
+ * @param resource $mysql_link an resource object link to the database
+ * @param array $userinformations
+ * 
+ * @return bool TRUE on success FALSE on failure
+ */
+function insert_new_usr($mysql_link, $userinformations = array()) {
+    list($username, $firstname, $lastname,$email, $password)=$userinformations;
+    
+    $SQL = "INSERT INTO `users`
+    (`username`, `first_name`, `last_name`, `mail`, `password`) 
+    VALUES 
+    '$username','$firstname','$lastname','$email','$password'";
+
+    if($mysqli_query($mysql_link, $SQL)) {
+        return TRUE;
+    }
+    else {
+        return FALSE;
+    }
+   
+}
+
+/**
+ * Check to see if the user is logged in
+ * 
+ * @param $username the user that is beeing checked
+ * @param $sha_key the unique key of the user generated per session
+ * 
+ * @return bool TRUE if the user is logged in FALSE otherwise
+ */
+function is_usr_logged($username, $ses_key) {
+    if(isset($_SESSION[$username]) && isset($_SESSION[$ses_key])) {
+        return TRUE;
+    }
+    else {
+        return FALSE;
+    }
+    
+}
+/**
+ * Generate unique string
+ * 
+ * @param string $username username for witch the unique string is generated
+ * 
+ * @return string the unique sha1 string
+ */
+function generate_unique_str($username=NULL) {
+        return sha1($username.uniqid());
+}
+
+/**
+ * Start the session
+ * 
+ * @param void
+ * 
+ * @return mixed start session or False on error
+ */
+function initialize_session() {
+    if(!isset($_SESSION)){
+        session_start();
+    }
+    else {
+        return FALSE;
+    }
+}
+
+/**
+ * Destroy the sessin
+ * 
+ * @param void
+ * 
+ * @return mixed bool FALSE on error or destroy the session
+ */
+function destroy_session() {
+    if(isset($_SESSION)){
+        //TODO: see php manual, add session cookie deletion
+        // after that destroy the session
+        session_destroy();
+    }
+    else {
+        return FALSE;
+    }
+}
