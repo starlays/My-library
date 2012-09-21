5d43f493b64372551a8c5ca1ea3aac4487e7073b
Lazar Florentin <florentin.lazar@gmail.com>
2012-09-21 15:20:57 +0300

    modified module_holder to reflect the folder structure

--- src/module_holder.php
+++ src/module_holder.php
@@ -17,58 +17,48 @@ return array (
     'user'      => array (
         'title'         => 'User page',
         'in_menu'       => 1,
-        'pre-processing'=> array(
-               'session'=>'session_start.php',
-        ),
         'content_VL'    => 'user.php',
         'content_BL'    => 'user_bl.php',
         'depend'        => array('books'),
-        'resources'     => array('mysql')
+        'resources'     => array('mysql', 'authentication')
     ),
     'admin'     => array (
         'title'         => 'Admin page',
         'in_menu'       => 1,
         'content_VL'    => 'admin.php',
-        'resources'     => array('mysql')
+        'resources'     => array('mysql', 'authentication')
     ),
      'register' => array (
         'title'         => 'Register',
         'in_menu'       => 2,
         'content_VL'    => 'register.php',
-        'resources'     => array('mysql','register')
+        'content_BL'    => 'register_BL.php',
+        'resources'     => array('mysql', 'authentication')
     ),
      'login'    => array (
         'title'         => 'Login',
         'in_menu'       => 2,
-        'pre-processing'      => array(
-            'session' => 'session_start.php',
-        ),
-        'resources'     => array('mysql','login','autologin'),
         'content_VL'    => 'login.php',
+        'content_BL'    => 'login_bl.php',
+        'resources'     => array('mysql', 'authentication')
     ),
      'recover'  => array (
         'title'         => 'Recover',
         'in_menu'       => 2,
-        'content_VL'    => 'recover.php',
-        'resources'     => array('mysql')
+        'content_VL'    => 'recover.php', //where is the file?
+        'resources'     => array('mysql', 'authentication')
     ),
      'logout'   => array (
         'title'         => 'Logout',
         'in_menu'       => 2,
-        'pre-processing'      => array(
-            'session' => 'session_destroy.php',
-        ),
         'content_VL'    => 'logout.php',
+        'resources'     => array('authentication')
     ),
     'upload'   => array(
         'title'      => 'File upload',
         'in_menu'    => 1,
-        'pre-processing'      => array(
-            'session' => 'session_start.php',
-        ),
         'content_VL' => 'upload.php',
         'content_BL' => 'upload_bl.php',
-        'resources'     => array('mysql','login','autologin'),
-        
+        'resources'     => array('mysql', 'authentication')
     )
 );
