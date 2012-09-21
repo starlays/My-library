f63ee39dcf0d6ec6ed9eb187081b3166ea237937
Lazar Florentin <florentin.lazar@gmail.com>
2012-09-21 11:28:21 +0300

    refactor login module business logic file

--- src/modules/login/login_bl.php
+++ src/modules/login/login_bl.php
@@ -1,40 +1,47 @@
 <?php
-//TODO: data validation
-const ERR_PASSNOMATCH   = 60;
-const ERR_USRPWD    = 61;
-const MSG_LOGINOK = 62;
-$err = NULL;
-$msg = NULL;
+/**
+ * Error constants for module authentication 
+ */
+const ERR_AUTH_MISSINFO  = 50;
+const ERR_AUTH_NOUSER    = 51;
+const ERR_AUTH_RETRVINFO = 52;
+const ERR_AUTH_STARTSESS = 53;
 
 if(isset($_POST['login'])){
     $reginfo = array($_POST['usr'], $_POST['pwd']);
     
-    if(isEmpty($reginfo)) {
-        list($username,$pass) = datafilter($reginfo);
-        
-        $SQL = "SELECT id AS uid, username, first_name, last_name, mail AS e_mail
-                    FROM `users` WHERE username='$username' AND password='$pass';";
-        $result = mysqli_query($mysql_link,$SQL);
-        //If we have an result
-        if($userdata = mysqli_fetch_assoc($result)){
+    if(!isEmpty_array_vals($reginfo)) {
+        list($username, $pass) = datafilter($reginfo);
 
-            $_SESSION['is_logged_in']   = 1;
-            $_SESSION['user_ID']        = $userdata['uid'];
-            $_SESSION['username']       = $userdata['username'];
-            $_SESSION['first_name']     = $userdata['first_name'];
-            $_SESSION['last_name']      = $userdata['last_name'];
-            
-            unset($userdata);
-            mysqli_close($mysql_link);
-            
-            return MSG_LOGINOK;
+        if(user_exists($mysql_link, $username, $pass)){
+            $SQL = "SELECT id AS uid, username, first_name, last_name, mail AS e_mail
+                    FROM `users` WHERE username='$username' AND password='$pass';";
+            if($userdata = retrive_assoc($mysql_link, $SQL)) {
+                if(initialize_session()) {
+                    $ses_key = generate_unique_str($username);
+
+                    $_SESSION[$ses_key]  = $ses_key;
+                    $_SESSION[$username] = $username;
+                    $_SESSION['user_ID'] = $userdata['uid'];
+                    $_SESSION['first_name']     = $userdata['first_name'];
+                    $_SESSION['last_name']      = $userdata['last_name'];
+
+                    unset($userdata);
+                    mysqli_close($mysql_link);
+                }
+                else {
+                    return ERR_AUTH_STARTSESS;
+                }
+            }
+            else {
+                return ERR_AUTH_RETRVINFO;
+            }
         }
         else {
-            return ERR_PASSNOMATCH;
+            return ERR_AUTH_NOUSER;
         }
     }
     else {
-        return ERR_USRPWD;
+        return ERR_AUTH_MISSINFO;
     }
 }
-
