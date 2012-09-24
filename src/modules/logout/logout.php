2d1a13e1d04816401a58af4ed2736d807e98742a
Lazar Florentin <florentin.lazar@gmail.com>
2012-09-24 20:48:37 +0300

    add code in module logout VL file

--- src/modules/logout/logout.php
+++ src/modules/logout/logout.php
@@ -1,5 +1,11 @@
-<?php
-/**
- * Logout file
- */
-destroy_session();
+<?
+
+if(is_numeric($page_vl_vars)) {
+    switch($page_vl_vars) {
+        case LOGOUT_CANNOTSTOP:
+            echo sprintf('Can\'t stop the session! Error: %d', LOGOUT_CANNOTSTOP);
+            break;
+        case LOGOUT_SUCCESS:
+            echo 'You are now logged out';
+    }
+}
