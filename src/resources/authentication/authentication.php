56b772131c382bda32380eca02ae49d7c1fe61e5
Lazar Florentin <florentin.lazar@gmail.com>
2012-09-24 22:57:23 +0300

    Merge authentication.php

--- src/resources/authentication/authentication.php
+++ src/resources/authentication/authentication.php
@@ -10,17 +10,17 @@
 /**
  * Check to se if the given user exists in dadabase
  * user_exists($mysql_link, $username,[$password])
- * 
+ *
  * @param resource $mysql_link an resource object link to the database
  * @param string $username the user that you wish to check if exists
  * @param string $password optional param the password attached to the $username
- * 
+ *
  * @return bool TRUE if user exists and otherwise FALSE
  */
 function user_exists($mysql_link, $username=NULL, $password=NULL) {
-    
+
     $SQL = "SELECT username FROM `users` WHERE username='$username'";
-  
+
     if(func_num_args()===3) {
         if(is_string(func_get_arg(2))) {
             $SQL.= " AND password='$password'";
@@ -40,15 +40,15 @@ function user_exists($mysql_link, $username=NULL, $password=NULL) {
  *
  * @param resource $mysql_link an resource object link to the database
  * @param array $userinformations
- * 
+ *
  * @return bool TRUE on success FALSE on failure
  */
 function insert_new_usr($mysql_link, $userinformations = array()) {
     list($username, $firstname, $lastname,$email, $password)=$userinformations;
-    
+
     $SQL = "INSERT INTO `users`
-    (`username`, `first_name`, `last_name`, `mail`, `password`) 
-    VALUES 
+    (`username`, `first_name`, `last_name`, `mail`, `password`)
+    VALUES
     '$username','$firstname','$lastname','$email','$password'";
 
     if($mysqli_query($mysql_link, $SQL)) {
@@ -57,15 +57,15 @@ function insert_new_usr($mysql_link, $userinformations = array()) {
     else {
         return FALSE;
     }
-   
+
 }
 
 /**
  * Check to see if the user is logged in
- * 
+ *
  * @param $username the user that is beeing checked
  * @param $sha_key the unique key of the user generated per session
- * 
+ *
  * @return bool TRUE if the user is logged in FALSE otherwise
  */
 function is_usr_logged($username, $ses_key) {
@@ -75,13 +75,13 @@ function is_usr_logged($username, $ses_key) {
     else {
         return FALSE;
     }
-    
+
 }
 /**
  * Generate unique string
- * 
+ *
  * @param string $username username for witch the unique string is generated
- * 
+ *
  * @return string the unique sha1 string
  */
 function generate_unique_str($username=NULL) {
@@ -90,9 +90,9 @@ function generate_unique_str($username=NULL) {
 
 /**
  * Start the session
- * 
+ *
  * @param void
- * 
+ *
  * @return mixed start session or False on error
  */
 function initialize_session() {
@@ -106,13 +106,24 @@ function initialize_session() {
 
 /**
  * Destroy the sessin
- * 
+ *
  * @param void
- * 
+ *
  * @return mixed bool FALSE on error or destroy the session
  */
 function destroy_session() {
-    session_start();
-    session_unset();
-    session_destroy();
+    if(session_start()) {
+        if(isset($_SESSION)){
+            //TODO: see php manual, add session cookie deletion
+            // after that destroy the session
+            session_unset();
+            session_destroy();
+        }
+        else {
+            return FALSE;
+        }
+    }
+    else {
+        return FALSE;
+    }
 }
