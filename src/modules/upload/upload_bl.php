27c259e4b2d7a5bc5c312553058c237b7bfddf8a
Lazar Florentin <florentin.lazar@gmail.com>
2012-09-25 15:13:54 +0300

    implemented module upload BL file

--- src/modules/upload/upload_bl.php
+++ src/modules/upload/upload_bl.php
@@ -3,17 +3,52 @@
  * @group FileUpload File upload
  */
 
-if(isset($_POST['file_upload'])) {
-    $upld_file = $_FILES['file'];
-    
-    if($upld_file['error'] === UPLOAD_ERR_OK) {
-         if(is_uploaded_file($upld_file['tmp_name'])){
-             // here process if the user is loged in and if it is create a dir
-             // inside uploads with dir name using his uuid
-             // check if file is uploaded
-             // move file in to user dir
-             // return status codes
-         }
+/**
+ * Constants returned by upload module
+ */
+const UPLD_ERR_SESSION    = 115;
+const UPLD_NOT_LOGGED_IN  = 116;
+const UPLD_ERR_NOWRITEDIR = 117;
+const UPLD_ERR_MKDIR      = 118;
+const UPLD_SUCCESS        = 119;
+
+if(initialize_session()) {
+    if(isset($_SESSION['username']) && is_usr_logged($_SESSION['username'])) {
+        
+        if(isset($_POST['file_upload'])) {
+            $upld_file = $_FILES['file'];
+
+            if($upld_file['error'] === UPLOAD_ERR_OK) {
+                 if(is_uploaded_file($upld_file['tmp_name'])){
+                     $usr_upld_dir = __UPLOADS__.$_SESSION['user_ID'];
+                     
+                     if(is_dir($usr_upld_dir)) {
+                         if(!is_writable($usr_upld_dir)) {
+                             
+                             return UPLD_ERR_NOWRITEDIR;
+                         }
+                     }
+                     else {
+                         umask(0003);
+                         if(!mkdir($usr_upld_dir,0774)){
+                             
+                             return UPLD_ERR_MKDIR;
+                         }
+                     }
+                     if(move_uploaded_file($upld_file['tmp_name'], 
+                             $usr_upld_dir.DIRECTORY_SEPARATOR.$upld_file['name'])) {
+                         
+                         return UPLD_SUCCESS;
+                     }
+                     
+                 }
+            }
+        }
     }
-    
+    else {
+        return UPLD_NOT_LOGGED_IN;
+    }
+}
+else {
+    return UPLD_ERR_SESSION;
 }
