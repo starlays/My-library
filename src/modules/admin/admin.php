826f081e5381867e77fc896acf0aa2605466fd4e
George Dicu <dicugeorge1987@yahoo.com>
2012-09-26 18:22:04 +0300

    added VL code for admin module

---
+++ src/modules/admin/admin.php
@@ -0,0 +1,34 @@
+<?php
+if(isset($_SESSION['username']) && isset($_SESSION['ses_key'])) {
+    echo "<div>
+    <form action='' method='POST'>
+    <fieldset>
+    <legend> Add New User: </legend>
+    <label for='fn'>* First Name:</label><input id='fn' name='fn' type='text' /><br />
+    <label for='ln'>* Last Name:</label><input id='ln' name='ln' type='text' /><br />
+    <label for='usr'>* Username:</label><input id='usr' name='usr' type='text' /><br />
+    <label for='mail'>* E-mail:</label><input id='mail' name='mail' type='text' /><br />
+    <label for='pwd'>* Password:</label><input id='pwd' name='pwd' type='password' /><br />
+    <label for='rpwd'>* Retype Password:</label><input id='rpwd' name='rpwd' type='password' /><br />
+    </fieldset>
+    <input type='submit' name='register' value='Add User' />
+    </form>";
+}
+
+if(is_array($loaded_deps)) {
+    switch($loaded_deps[0]) {
+        case ERR_FIELDMISS:
+            echo 'Fields marked with * are necessary.';
+            break;
+        case ERR_PASSNOMATCH:
+            echo 'The passwords are not matching';
+            break;
+        case ERR_USEREXISTS:
+            echo 'This user already exists';
+            break;
+        case REGISTER_SUCCESS:
+            echo 'Registration Successful';
+    }
+}
+echo '</div>';
+
