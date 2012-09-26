0076a66113f9ca133204faec2e0fe9b40613ada9
George Dicu <dicugeorge1987@yahoo.com>
2012-09-26 15:53:14 +0300

    FIX undefined $username variable

--- src/modules/register/register_BL.php
+++ src/modules/register/register_BL.php
@@ -9,28 +9,35 @@ const REGISTER_SUCCESS = 55;
 const ERR_PASSNOMATCH  = 57;
 const ERR_USEREXISTS   = 58;
 const ERR_FIELDMISS    = 59;
+const USER_SESSIONOTSTART = 60;
 
-if(isset($_POST['register'])){
-    $reginfo = array($_POST['fn'], $_POST['ln'], $_POST['usr'], $_POST['mail'], 
-            $_POST['pwd'], $_POST['rpwd']);
+if(initialize_session()) {
+    if(isset($_POST['register'])){
+        $reginfo = array($_POST['fn'], $_POST['ln'], $_POST['usr'], $_POST['mail'], 
+                $_POST['pwd'], $_POST['rpwd']);
 
-    if(!isEmpty_array_vals($reginfo)) {
-        if($_POST['pwd'] === $_POST['rpwd']) {
-            if(!user_exists($mysql_link, $username)) {
-                if(insert_new_usr($mysql_link, $reginfo)){
-                    
-                    return REGISTER_SUCCESS;
+        if(!isEmpty_array_vals($reginfo)) {
+            if($_POST['pwd'] === $_POST['rpwd']) {
+                $username = $_POST['usr'];
+                if(!user_exists($mysql_link, $username)) {
+                    if(insert_new_usr($mysql_link, $reginfo)){
+                        
+                        return REGISTER_SUCCESS;
+                    }
+                }
+                else {
+                    return ERR_USEREXISTS;
                 }
             }
             else {
-                return ERR_USEREXISTS;
+                return ERR_PASSNOMATCH;
             }
         }
         else {
-            return ERR_PASSNOMATCH;
+            return ERR_FIELDMISS;
         }
     }
-    else {
-        return ERR_FIELDMISS;
-    }
+}
+else {
+    return USER_SESSIONOTSTART;
 }
