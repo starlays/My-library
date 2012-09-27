9664ddb66a066b2500ed7ebb7c31ae6afc452a7b
George Dicu <>
2012-09-28 00:57:49 +0300

    issue #11 fix, resource loader

--- src/resources_loader.php
+++ src/resources_loader.php
@@ -5,7 +5,7 @@ $resourcess_to_load = array();
 if(isset($modules[$page]['resources'])) {
 
     foreach($modules[$page]['resources'] as $resource) {
-        $res_path = __RESOURCES__.$resource.DIRECTORY_SEPARATOR;
+        $res_path = __RESOURCES__.$resource.D_S;
 
         if(isset($resources[$resource]['resource_file']) &&
             check_file($resources[$resource]['resource_file'], $res_path)) {
