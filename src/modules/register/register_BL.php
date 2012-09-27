de778d7c13a523ec8483cd1d510f870128262a75
George Dicu <>
2012-09-28 00:38:10 +0300

    issue #10 fix, don't relay on data order in a dictionary register_bl.php

--- src/modules/register/register_BL.php
+++ src/modules/register/register_BL.php
@@ -13,13 +13,18 @@ const USER_SESSIONOTSTART = 60;
 
 if(initialize_session()) {
     if(isset($_POST['register'])){
-        $reginfo = array($_POST['fn'], $_POST['ln'], $_POST['usr'], $_POST['mail'], 
-                $_POST['pwd'], $_POST['rpwd']);
+        $reginfo = array(
+                        'fn'  => $_POST['fn'],
+                        'ln'  => $_POST['ln'],
+                        'usr' =>$_POST['usr'],
+                        'mail' => $_POST['mail'], 
+                        'pwd' => $_POST['pwd'],
+                        'rpwd' => $_POST['rpwd'],
+                    );
 
         if(!isEmpty_array_vals($reginfo)) {
             if($_POST['pwd'] === $_POST['rpwd']) {
-                $username = $_POST['usr'];
-                if(!user_exists($mysql_link, $username)) {
+                if(!user_exists($mysql_link, $reginfo['usr'])) {
                     if(insert_new_usr($mysql_link, $reginfo)){
                         
                         return REGISTER_SUCCESS;
