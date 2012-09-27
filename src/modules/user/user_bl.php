d64dd99fab1ded47ec3305a80b902cdee6f88067
George Dicu <>
2012-09-27 19:38:14 +0300

    fix cross platform compatibility,fix escaped the img&ebook path

--- src/modules/user/user_bl.php
+++ src/modules/user/user_bl.php
@@ -7,7 +7,7 @@ const USER_SESSIONOTSTART = 43;
 const USER_ERR_UPLOAD     = 44;
 const USER_NOT_LOGGED_IN  = 45;
 
-initialize_session();
+if (initialize_session()){
     if(isset($_SESSION['username']) && isset($_SESSION['ses_key'])
                                     && is_usr_logged($_SESSION['username'])) {
         if(isset($_POST['usr_add_book'])) {
@@ -32,15 +32,17 @@ initialize_session();
                         mysqli_real_escape_string($mysql_link, $data);
                 }
 
-                $cvr_upld_dir   = __UPLOADS__.$_POST['book_title'].DIRECTORY_SEPARATOR.'cvr_img'.DIRECTORY_SEPARATOR;
-                $ebook_upld_dir = __UPLOADS__.$_POST['book_title'].DIRECTORY_SEPARATOR.'ebook'.DIRECTORY_SEPARATOR;
-
-                if(!is_null($book_cvrimg)) {
+                $cvr_upld_dir   =mysqli_real_escape_string($mysql_link,
+                        __UPLOADS__.$_POST['book_title'].DIRECTORY_SEPARATOR.'cvr_img'.DIRECTORY_SEPARATOR);
+                $ebook_upld_dir = mysqli_real_escape_string($mysql_link, 
+                        __UPLOADS__.$_POST['book_title'].DIRECTORY_SEPARATOR.'ebook'.DIRECTORY_SEPARATOR);
+                
+                if(!is_null($book_cvrimg) && !empty($book_cvrimg['name'])) {
                     if(!user_upload($book_cvrimg, $cvr_upld_dir)){
                         return USER_ERR_UPLOAD;
                     }
                 }
-                if(!is_null($book_ebook)) {
+                if(!is_null($book_ebook) && !empty($book_ebook['name'])) {
                     if(!user_upload($book_ebook, $ebook_upld_dir)){
                         return USER_ERR_UPLOAD;
                     }
@@ -72,24 +74,28 @@ initialize_session();
                 mysqli_close($mysql_link);
             }
         }
-        
-    }
-    elseif(isset($_POST['search_book'])) {
-        if(isset($_POST['search_title'])) {
-            $search_title = $_POST['search_title'];
+        elseif(isset($_POST['search_book'])) {
+            if(isset($_POST['search_title'])) {
+                $search_title = $_POST['search_title'];
 
-            
-            $sql_sbooks = "SELECT title AS book_title, name AS author_name, description AS book_description,
-            insert_date AS book_insert_date, cvr_img_path AS book_cvr_img_path, e_book_path AS book_ebook_path
-             FROM `books` INNER JOIN `authors` ON books.id_author = authors.id  WHERE title REGEXP '".$search_title."';";
 
-            $query = mysqli_query($mysql_link, $sql_sbooks);
-            return mysqli_fetch_row($query);
-            mysqli_close($mysql_link);
+                $sql_sbooks = "SELECT title AS book_title, name AS author_name, 
+                    description AS book_description,
+                insert_date AS book_insert_date, cvr_img_path AS book_cvr_img_path,
+                e_book_path AS book_ebook_path
+                 FROM `books` INNER JOIN `authors` ON books.id_author = authors.id 
+                 WHERE title REGEXP '".$search_title."';";
+
+                $query = mysqli_query($mysql_link, $sql_sbooks);
+                return mysqli_fetch_row($query);
+                mysqli_close($mysql_link);
+             } 
         }
-    } 
+    }
     else {
         return USER_NOT_LOGGED_IN;
     }
-
-
+}
+else {
+    return USER_SESSIONOTSTART;
+}
