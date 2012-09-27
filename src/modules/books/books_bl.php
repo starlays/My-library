3b908cb945e72a22470acb5278aafbd153d00fab
George Dicu <>
2012-09-27 14:57:46 +0300

    slight changes

--- src/modules/books/books_bl.php
+++ src/modules/books/books_bl.php
@@ -1,16 +1,32 @@
 <?php
-// if we have an mysql connection retrive the ncessary data out of the database
-if($mysql_link) {
-    
-    $SQL = "SELECT 
-        title AS book_title, name AS author_name, description AS book_description,
-        insert_date AS book_insert_date FROM `books`
-        INNER JOIN `authors` ON books.id_author = authors.id;";
-    
-    if($result = mysqli_query($mysql_link, $SQL)) {
-        if($books = mysqli_fetch_all($result, MYSQLI_ASSOC)) {
-            mysqli_free_result($result);
-            return $books;            
-        }  
-    }   
-}
+
+// no need to add an if construct, because user module depend on this
+// and that module too starts a sesion!
+// but if this module start as its own it nust start a sesion!
+initialize_session();
+    // if we have an mysql connection retrive the ncessary data out of the database
+    if($mysql_link) {
+
+        $uID = $_SESSION['user_ID'];
+        $default_order = 'title';
+        $default_asc = 'ASC';
+        
+        if(isset($_POST['reorder'])) {
+            $default_order = $_POST['ordering'];
+            $default_asc = $_POST['asc'];
+        }
+        
+        $SQL = "SELECT 
+            title AS book_title, name AS author_name, description AS book_description,
+            insert_date AS book_insert_date FROM `books`
+            INNER JOIN authors ON books.id_author = authors.id 
+                WHERE id_insert_user = ".$uID." 
+                    ORDER BY ".$default_order." ".$default_asc.";";
+        
+        if($result = mysqli_query($mysql_link, $SQL)) {
+            if($books = mysqli_fetch_all($result, MYSQLI_ASSOC)) {
+                mysqli_free_result($result);
+                return $books;
+            }
+        }
+    }
