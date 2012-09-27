c5e2f38f3a0f2ef41f236562d343e1c876a11ff9
George Dicu <>
2012-09-27 14:41:01 +0300

    Added new form so that user can select the ordering of it's own books!

--- src/modules/books/books.php
+++ src/modules/books/books.php
@@ -1,16 +1,41 @@
-<div id="books">
 <?php
-if(isset($page_vl_vars) && is_array($page_vl_vars)) {
-    foreach($page_vl_vars as $informations) {
-        echo '<div id="group_book">';
-        foreach($informations as $metadata => $data){
-            echo   $metadata, ' : ',$data, ' ';
+
+if(isset($_SESSION['username']) && isset($_SESSION['ses_key'])){ 
+    echo "<div id='books'>
+    <form action='' method='POST'>
+    <fieldset>
+    <legend> Order your books by: </legend>
+    <label for='title'>Title:</label>
+        <input type='radio' name='ordering' value='title' checked='yes'/>
+      
+    <label for='author'>Author:</label>
+        <input type='radio' name='ordering' value='authors' />
+        
+    <label for='description'>Description:</label>
+        <input type='radio' name='ordering' value='description' /><br />
+               
+    <label for='asc'>Asc:</label>
+        <input type='radio' name='asc' value='ASC' checked='yes'/>
+    <label for='desc'>Desc:</label>
+        <input type='radio' name='asc' value='DESC' /><br />
+    <input type='submit' name='reorder' value='Reorder' />
+    </fieldset>
+    </form>
+    </div>
+    ";
+    
+    if(isset($page_vl_vars) && is_array($page_vl_vars)) {
+        foreach($page_vl_vars as $informations) {
+            echo '<div id="group_book">';
+            foreach($informations as $metadata => $data){
+                echo   $metadata, ' : ',$data, ' ';
+            }
+            echo '</div>';
         }
-        echo '</div>';
+    }
+    else { 
+        echo 'You have no books corelated to your account.';
     }
 }
-else { 
-    echo 'No books to show.';
-}
-?>
-</div>
+
+
