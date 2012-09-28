34f24d23c0ef69727ab942b122aa342c160178c8
Lazar Florentin <florentin.lazar@gmail.com>
2012-09-28 11:31:28 +0300

    user input is now filtered

--- src/modules/register/register_BL.php
+++ src/modules/register/register_BL.php
@@ -14,13 +14,14 @@ const USER_SESSIONOTSTART = 60;
 if(initialize_session()) {
     if(isset($_POST['register'])){
         $reginfo = array(
-                        'fn'  => $_POST['fn'],
-                        'ln'  => $_POST['ln'],
-                        'usr' =>$_POST['usr'],
+                        'fn'   => $_POST['fn'],
+                        'ln'   => $_POST['ln'],
+                        'usr'  => $_POST['usr'],
                         'mail' => $_POST['mail'], 
-                        'pwd' => $_POST['pwd'],
+                        'pwd'  => $_POST['pwd'],
                         'rpwd' => $_POST['rpwd'],
                     );
+        $reginfo = datafilter($reginfo);
 
         if(!isEmpty_array_vals($reginfo)) {
             if($_POST['pwd'] === $_POST['rpwd']) {
