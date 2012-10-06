1e83f993328779ddbc60b8896dfe16c199199e69
Lazar Florentin <florentin.lazar@gmail.com>
2012-10-06 14:12:19 +0300

    - add books and status code variable container - books bl file now uses resource books - status codes and books are now returned as associative array

--- src/modules/books/books_bl.php
+++ src/modules/books/books_bl.php
@@ -1,35 +1,44 @@
 <?php
+/**
+ * books module
+ */
+/**
+ * Constants
+ */
+const BOOKS_NOT_LOGGED = 340;
+/**
+ * User book list conainer
+ */
+$books = NULL;
+/**
+ * Status code container
+ */
+$status_code = NULL;
 
-// no need to add an if construct, because user module depend on this
-// and that module too starts a sesion!
-// but if this module start as its own it nust start a sesion!
 if (initialize_session()){
     if(isset($_SESSION['username']) && isset($_SESSION['ses_key'])
                                 && is_usr_logged($_SESSION['username'])) {
         // if we have an mysql connection retrive the ncessary data out of the database
-    if($mysql_link) {
+        if($mysql_link) {
 
-        $uID = $_SESSION['user_ID'];
-        $default_order = 'title';
-        $default_asc = 'ASC';
-        
-        if(isset($_POST['reorder'])) {
-            $default_order = $_POST['ordering'];
-            $default_asc = $_POST['asc'];
-        }
-        
-        $SQL = "SELECT 
-            title AS book_title, name AS author_name, description AS book_description,
-            insert_date AS book_insert_date FROM `books`
-            INNER JOIN authors ON books.id_author = authors.id 
-                WHERE id_insert_user = ".$uID." 
-                    ORDER BY ".$default_order." ".$default_asc.";";
-        
-        if($result = mysqli_query($mysql_link, $SQL)) {
-            if($books = mysqli_fetch_all($result, MYSQLI_ASSOC)) {
-                mysqli_free_result($result);
-                return $books;
+            $uID = $_SESSION['user_ID'];
+            $default_order = 'title';
+            $default_asc   = 'ASC';
+
+            if(isset($_POST['reorder'])) {
+                $default_order = $_POST['ordering'];
+                $default_asc   = $_POST['asc'];
             }
+            
+            $books = retrive_user_books($mysql_link, $uID, $default_order, $default_asc);
         }
-    }}
+    }
+    else {
+        $status_code = BOOKS_NOT_LOGGED; 
+    }
 }
+
+return array(
+    'status_code' => $status_code,
+    'books'       => $books
+    );
