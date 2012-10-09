6a0ffad6a23be2c13c0fb5079e66cf63d0bbe83a
Lazar Florentin <florentin.lazar@gmail.com>
2012-10-09 21:29:49 +0300

    Books BL: - now checks uploaded file mime type - returns e_book files and cvr_images

--- src/modules/books/books_bl.php
+++ src/modules/books/books_bl.php
@@ -14,13 +14,26 @@ $books = NULL;
  * Status code container
  */
 $status_code = NULL;
+/**
+ * Allowed image mime type
+ */
+$image_mime = array('image/pjpeg', 'image/jpeg', 'image/gif', 
+                    'image/bmp', 'image/x-png', 'image/png');
+/**
+ * Allowed ebook mime type
+ */
+$ebook_mime = array('application/pdf');
+/**
+ * Container for the colected files
+ */
+$books_covers = $ebook_doc = NULL;
 
 if (initialize_session()){
     if(isset($_SESSION['username']) && isset($_SESSION['ses_key'])
                                 && is_usr_logged($_SESSION['username'])) {
         // if we have an mysql connection retrive the ncessary data out of the database
         if($mysql_link) {
-
+            
             $uID = $_SESSION['user_ID'];
             $default_order = 'title';
             $default_asc   = 'ASC';
@@ -29,8 +42,17 @@ if (initialize_session()){
                 $default_order = $_POST['ordering'];
                 $default_asc   = $_POST['asc'];
             }
-            
             $books = retrive_user_books($mysql_link, $uID, $default_order, $default_asc);
+
+            foreach($books as $book) {
+                 if(check_dir($book['cvr_img_path'])){
+                    $books_covers[$book['book_title']] = files_scand_dir($book['cvr_img_path'], $image_mime);
+
+                }
+                if(check_dir($book['e_book_path'])) {
+                    $ebook_doc[$book['book_title']] = files_scand_dir($book['e_book_path'], $ebook_mime);
+                }
+            }
         }
     }
     else {
@@ -39,6 +61,8 @@ if (initialize_session()){
 }
 
 return array(
-    'status_code' => $status_code,
-    'books'       => $books
+    'status_code'  => $status_code,
+    'books'        => $books,
+    'books_covers' => $books_covers,
+    'ebook_doc'    => $ebook_doc
     );
