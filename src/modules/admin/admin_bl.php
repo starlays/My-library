5c8f4cca9046391ef61517cee6f84e0ab7e86098
George Dicu <dicu.george007@gmail.com>
2012-10-09 20:47:16 +0300

    add verifcation if user is admin, admin_bl

--- src/modules/admin/admin_bl.php
+++ src/modules/admin/admin_bl.php
@@ -13,7 +13,8 @@ const ERR_PASSNOMATCH     = 451;
 const ERR_USEREXISTS      = 452;
 const ERR_FIELDMISS       = 453;
 const USER_SESSIONOTSTART = 454;
-const ADM_NOTLOGGED       = 455;
+const ERR_USRNOTLOGGED    = 455;
+const ERR_USRISNOADMIN    = 456;
 /**
  * Status code container
  */
@@ -22,39 +23,45 @@ $status_code = NULL;
 if(initialize_session()) {
     if(isset($_SESSION['username']) && isset($_SESSION['ses_key'])
                                     && is_usr_logged($_SESSION['username'])) {
-        if(isset($_POST['add_user'])){
-            $reginfo = array(
-                            'fn'   => $_POST['fn'],
-                            'ln'   => $_POST['ln'],
-                            'usr'  => $_POST['usr'],
-                            'mail' => $_POST['mail'], 
-                            'pwd'  => $_POST['pwd'],
-                            'rpwd' => $_POST['rpwd'],
-                        );
-            $reginfo = datafilter($reginfo);
+        
+        if(is_admin($mysql_link, $_SESSION['username'])){
+            if(isset($_POST['add_user'])){
+                $reginfo = array(
+                                'fn'   => $_POST['fn'],
+                                'ln'   => $_POST['ln'],
+                                'usr'  => $_POST['usr'],
+                                'mail' => $_POST['mail'], 
+                                'pwd'  => $_POST['pwd'],
+                                'rpwd' => $_POST['rpwd'],
+                            );
+                $reginfo = datafilter($reginfo);
 
-            if(!isEmpty_array_vals($reginfo)) {
-                if($_POST['pwd'] === $_POST['rpwd']) {
-                    if(!user_exists($mysql_link, $reginfo['usr'])) {
-                        if(insert_new_usr($mysql_link, $reginfo)){
-                            $status_code = REGISTER_SUCCESS;
+                if(!isEmpty_array_vals($reginfo)) {
+                    if($_POST['pwd'] === $_POST['rpwd']) {
+                        if(!user_exists($mysql_link, $reginfo['usr'])) {
+                            if(insert_new_usr($mysql_link, $reginfo)){
+                                $status_code = REGISTER_SUCCESS;
+                            }
+                        }
+                        else {
+                            $status_code = ERR_USEREXISTS;
                         }
                     }
                     else {
-                        $status_code = ERR_USEREXISTS;
+                        $status_code = ERR_PASSNOMATCH;
                     }
                 }
                 else {
-                    $status_code = ERR_PASSNOMATCH;
+                    $status_code = ERR_FIELDMISS;
                 }
             }
-            else {
-                $status_code = ERR_FIELDMISS;
-            }
+        }
+        else {
+            $status_code = ERR_USRISNOADMIN;
         }
     }
     else {
-        $status_code = ADM_NOTLOGGED;
+        $status_code = ERR_USRNOTLOGGED;
     }
 }
 else {
