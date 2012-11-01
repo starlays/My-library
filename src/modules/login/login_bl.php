266beb3af600515f41d9d0ac122c0d3823c78085
George Dicu <dicu.george007@gmail.com>
2012-11-01 18:48:24 +0200

    fix #16

--- src/modules/login/login_bl.php
+++ src/modules/login/login_bl.php
@@ -70,14 +70,10 @@ else{
  */
 if(isset($_POST['logout'])){
     
-    if(destroy_session()) {
-        //TODO: use a header to manipulate page redirect
-        //header("Location ".$_SERVER['SCRIPT_FILENAME']);
-        $status_code = LOGOUT_SUCCESS;
-    }
-    else {
-        $status_code = LOGOUT_CANNOTSTOP;
-    }
+    session_unset();
+    session_destroy();
+    $status_code = LOGOUT_SUCCESS;
+
 }
 
 return array('status_code' => $status_code);
