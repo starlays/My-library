90b9cb9e20bfbeaf47d61cb5d578efd12519e752
George Dicu <dicu.george007@gmail.com>
2012-11-01 18:59:37 +0200

    database fix

--- docs/database/mylibrary.sql
+++ docs/database/mylibrary.sql
@@ -3,9 +3,9 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 31, 2012 at 11:22 PM
--- Server version: 5.5.28-log
--- PHP Version: 5.4.8
+-- Generation Time: Nov 01, 2012 at 06:13 PM
+-- Server version: 5.5.25a
+-- PHP Version: 5.4.4
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,7 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `mylibrary`
 --
-DROP DATABASE `mylibrary`;
 CREATE DATABASE `mylibrary` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `mylibrary`;
 
@@ -29,6 +28,7 @@ USE `mylibrary`;
 -- Table structure for table `admin_msg`
 --
 
+DROP TABLE IF EXISTS `admin_msg`;
 CREATE TABLE IF NOT EXISTS `admin_msg` (
   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `id_admin` mediumint(9) NOT NULL,
@@ -37,12 +37,6 @@ CREATE TABLE IF NOT EXISTS `admin_msg` (
   PRIMARY KEY (`id`),
   KEY `id_admin` (`id_admin`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- RELATIONS FOR TABLE `admin_msg`:
---   `id_admin`
---       `users` -> `id`
---
 
 --
 -- Dumping data for table `admin_msg`
@@ -57,12 +51,13 @@ INSERT INTO `admin_msg` (`id`, `id_admin`, `message`, `date`) VALUES
 -- Table structure for table `authors`
 --
 
+DROP TABLE IF EXISTS `authors`;
 CREATE TABLE IF NOT EXISTS `authors` (
   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `name` varchar(20) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `id` (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=22 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=23 ;
 
 --
 -- Dumping data for table `authors`
@@ -88,7 +83,8 @@ INSERT INTO `authors` (`id`, `name`) VALUES
 (18, 'tata'),
 (19, 'foo'),
 (20, 'asdasd'),
-(21, 'aaa');
+(21, 'aaa'),
+(22, 'mata');
 
 -- --------------------------------------------------------
 
@@ -96,6 +92,7 @@ INSERT INTO `authors` (`id`, `name`) VALUES
 -- Table structure for table `books`
 --
 
+DROP TABLE IF EXISTS `books`;
 CREATE TABLE IF NOT EXISTS `books` (
   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `title` char(20) NOT NULL,
@@ -112,17 +109,7 @@ CREATE TABLE IF NOT EXISTS `books` (
   KEY `id_rate` (`id_rate`,`id_insert_user`),
   KEY `id_author` (`id_author`),
   KEY `id_insert_user` (`id_insert_user`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=20 ;
-
---
--- RELATIONS FOR TABLE `books`:
---   `id_rate`
---       `rating_value` -> `id`
---   `id_insert_user`
---       `users` -> `id`
---   `id_author`
---       `authors` -> `id`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=21 ;
 
 --
 -- Dumping data for table `books`
@@ -130,9 +117,9 @@ CREATE TABLE IF NOT EXISTS `books` (
 
 INSERT INTO `books` (`id`, `title`, `id_author`, `description`, `insert_date`, `cvr_img_path`, `e_book_path`, `id_rate`, `id_insert_user`, `rights`) VALUES
 (15, 'Book', 19, 'bar', '2012-10-11', '/home/starlays/learning/My-library/webroot/uploads/Book/cvr_img/', '/home/starlays/learning/My-library/webroot/uploads/Book/ebook/', 1, 1, 7),
-(16, 'Book 3', 19, 'test', '2012-10-16', '/home/starlays/learning/My-library/webroot/uploads/Book 3/cvr_img/', '/home/starlays/learning/My-library/webroot/uploads/Book 3/ebook/', 1, 13, 7),
 (17, 'Book 4', 19, 'aaa', '2012-10-22', '/home/starlays/learning/My-library/webroot/uploads/Book 4/cvr_img/', '/home/starlays/learning/My-library/webroot/uploads/Book 4/ebook/', 1, 13, 3),
-(19, 'Book 5', 21, 'test', '2012-10-25', '/home/starlays/learning/My-library/webroot/uploads/Book 5/cvr_img/', '/home/starlays/learning/My-library/webroot/uploads/Book 5/ebook/', 1, 13, 1);
+(19, 'Book 5', 21, 'test', '2012-10-25', '/home/starlays/learning/My-library/webroot/uploads/Book 5/cvr_img/', '/home/starlays/learning/My-library/webroot/uploads/Book 5/ebook/', 1, 13, 1),
+(20, 'mata', 22, 'mata', '2012-11-01', 'E:\\webdev\\apache\\htdocs\\webroot\\uploads\\mata\\cvr_img\\', 'E:\\webdev\\apache\\htdocs\\webroot\\uploads\\mata\\ebook\\', 1, 13, 1);
 
 -- --------------------------------------------------------
 
@@ -140,6 +127,7 @@ INSERT INTO `books` (`id`, `title`, `id_author`, `description`, `insert_date`, `
 -- Table structure for table `rating_value`
 --
 
+DROP TABLE IF EXISTS `rating_value`;
 CREATE TABLE IF NOT EXISTS `rating_value` (
   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `value` tinyint(1) NOT NULL,
@@ -164,36 +152,28 @@ INSERT INTO `rating_value` (`id`, `value`) VALUES
 -- Table structure for table `user_book_rating`
 --
 
+DROP TABLE IF EXISTS `user_book_rating`;
 CREATE TABLE IF NOT EXISTS `user_book_rating` (
   `id_user` mediumint(9) NOT NULL,
   `id_rate` mediumint(9) NOT NULL,
   `id_book` mediumint(9) NOT NULL,
   KEY `id_user` (`id_user`,`id_rate`,`id_book`),
   KEY `id_book` (`id_book`),
-  KEY `id_rate` (`id_rate`)
+  KEY `id_rate` (`id_rate`),
+  KEY `id_book_2` (`id_book`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- RELATIONS FOR TABLE `user_book_rating`:
---   `id_user`
---       `users` -> `id`
---   `id_rate`
---       `rating_value` -> `id`
---
 
 --
 -- Dumping data for table `user_book_rating`
 --
 
 INSERT INTO `user_book_rating` (`id_user`, `id_rate`, `id_book`) VALUES
-(2, 4, 3),
-(2, 4, 3),
-(3, 3, 4),
-(3, 3, 4),
-(13, 1, 18),
-(13, 2, 19),
-(13, 4, 16),
-(13, 4, 17);
+(1, 3, 15),
+(2, 2, 15),
+(3, 2, 15),
+(4, 4, 15),
+(13, 5, 15),
+(15, 1, 15);
 
 -- --------------------------------------------------------
 
@@ -201,6 +181,7 @@ INSERT INTO `user_book_rating` (`id_user`, `id_rate`, `id_book`) VALUES
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
   `id` mediumint(9) NOT NULL AUTO_INCREMENT,
   `username` char(15) DEFAULT NULL,
@@ -224,12 +205,12 @@ INSERT INTO `users` (`id`, `username`, `first_name`, `last_name`, `mail`, `passw
 (1, 'foo', NULL, NULL, NULL, 'foobar', 0, 1, '', 0),
 (2, 'bar', NULL, NULL, NULL, 'barfoo', 0, 1, '', 0),
 (3, 'baz', 'Dicu', 'George', NULL, 'baz', 0, 1, '', 0),
-(4, 'test', 'test', 'test', 'test', 'test', 0, 1, '', 0),
-(8, 'newuser', 'newuser', 'newuser', 'newuser', 'newuser', 0, 1, '', 0),
+(4, 'test', 'test', 'test', 'test', 'test', 1, 1, '', 0),
+(8, 'newuser', 'newuser', 'newuser', 'newuser', 'newuser', 1, 1, '', 0),
 (12, 'usr', 'fn', 'ln', 'mail', 'psw', 0, 1, '', 0),
 (13, 'root', 'root', 'root', 'root', 'root', 0, 7, '', 1),
-(15, 'xao', 'Dicu', 'George', 'xao_geo007@yahoo.com', 'test', 0, 15, '0ff39bbbf981ac0151d340c9aa40e63e', 0),
-(16, 'barfoo', 'florin', 'test', 'foo@bar.com', 'barfoo', 0, 1, '289dff07669d7a23de0ef88d2f7129e7', 0);
+(15, 'xao', 'Dicu', 'George', 'xao_geo007@yahoo.com', 'test', 0, 3, '0ff39bbbf981ac0151d340c9aa40e63e', 1),
+(16, 'barfoo', 'florin', 'test', 'foo@bar.com', 'barfoo', 0, 3, '289dff07669d7a23de0ef88d2f7129e7', 0);
 
 --
 -- Constraints for dumped tables
@@ -253,5 +234,10 @@ ALTER TABLE `books`
 -- Constraints for table `user_book_rating`
 --
 ALTER TABLE `user_book_rating`
+  ADD CONSTRAINT `user_book_rating_ibfk_3` FOREIGN KEY (`id_book`) REFERENCES `books` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `user_book_rating_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `users` (`id`) ON DELETE CASCADE,
   ADD CONSTRAINT `user_book_rating_ibfk_2` FOREIGN KEY (`id_rate`) REFERENCES `rating_value` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
