ad3a8d09ca4f0a4dab772ad9cc507d57df4a806f
Lazar Florentin <florentin.lazar@gmail.com>
2013-10-18 00:00:57 +0300

    add controller for home page

---
+++ src/controllers/home.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Home page controller
+ */
+class homeController
+{
+    /**
+    * Variable that will hold the view of home page
+    */
+    public $template = 'home';
+
+    /**
+    * This function will handle the call from the router.php file 
+    *
+    * @param array $getVars
+    */
+    public function main(array $getVars)
+    {
+        // TODO: implement the function handle
+        var_dump($getVars);
+    }
+}
