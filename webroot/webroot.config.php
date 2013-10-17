d8d9a33fd362179cd2ea63d77d7b4ce28de2f150
Lazar Florentin <florentin.lazar@gmail.com>
2013-10-17 14:55:15 +0300

    add webroot configuration file; basic configuration for index file from webroot dir

---
+++ webroot/webroot.config.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Webroot configuration file
+ */
+
+/**
+ * Prevent this file direct access
+ */
+if(!defined('MyLibrar')) {
+    header("HTTP/1.1 403.8 Site access denied");
+    header("Status: 403.8 Site access denied");
+    exit('Access forbiten!');
+}
+/**
+ * Get direcotry separator specified by OS. Do not chage this.
+ */
+define('DS', DIRECTORY_SEPARATOR);
+/**
+ * The location where this file will be is the webroot.
+ * In webroot dir are located all the static assets
+ */
+define('WEBROOT', __DIR__.DS);
+ /**
+  * Application source file
+  */
+define('APPSRC', DS.'src'.DS)
