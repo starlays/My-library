1ac353d2179c59323610c5fb64d3565fb829e5b3
Lazar Florentin <florentin.lazar@gmail.com>
2013-10-18 00:02:53 +0300

    add basic decisions in router file

--- src/controllers/router.php
+++ src/controllers/router.php
@@ -2,4 +2,26 @@
 
 $request = $_SERVER['QUERY_STRING'];
 
+var_dump($request);
 // TODO: sanitarize and process request
+
+$target = APPSRC . DS.'controllers'.DS . $page . '.php';
+
+if(file_exists($target))
+{
+    include_once($target);
+    
+    $class = $page . 'Controller';
+
+    if(!class_exists($class))
+    {
+        exit('Class does not exist!');
+    }
+    
+    $controller = new $class;
+    $controller->main($getVars);
+}
+else
+{
+    exit('Page does not exist!');
+}
