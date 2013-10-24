caead7df3d8c7697a6fbeb5327776babe7babfeb
Lazar Florentin <florentin.lazar@gmail.com>
2013-10-24 19:31:19 +0300

    Add home model

---
+++ src/models/home.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * The model for home page, the back-end for home controller
+ */
+class homeModel
+{
+    public function __construct()
+    {
+        var_dump('Data processed inside the model');
+    }
+}
