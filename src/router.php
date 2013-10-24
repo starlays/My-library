1c94535aecd23158694ae9126a0e82db96779784
Lazar Florentin <florentin.lazar@gmail.com>
2013-10-24 19:22:46 +0300

    Moved router outside controllers dir

--- src/router.php
+++ src/router.php
@@ -1,11 +1,20 @@
 <?php
-
+/**
+ * This controller routes all incoming requests to the appropriate controller
+ */
+$getVars = array();
 $request = $_SERVER['QUERY_STRING'];
 
-var_dump($request);
+var_dump('Request: ',$request);
 // TODO: sanitarize and process request
-
+$parsed_query = explode('&', $request);
+// the first element is the page
+$page = array_shift($parsed_query);
 $target = APPSRC . DS.'controllers'.DS . $page . '.php';
+
+foreach($parsed_query as $vars) {
+    
+}
 
 if(file_exists($target))
 {
