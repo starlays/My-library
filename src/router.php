9e3a48009a69e62f6e8851febb51c36a6a8b986c
Lazar Florentin <florentin.lazar@gmail.com>
2013-10-29 19:05:46 +0200

    - router is autoloading the needed model classes - get parameters are now send to the page model for processing

--- src/router.php
+++ src/router.php
@@ -4,16 +4,25 @@
  */
 $getVars = array();
 $request = $_SERVER['QUERY_STRING'];
-
-var_dump('Request: ',$request);
 // TODO: sanitarize and process request
 $parsed_query = explode('&', $request);
 // the first element is the page
 $page = array_shift($parsed_query);
-$target = APPSRC . DS.'controllers'.DS . $page . '.php';
+//Automatically includes files containing classes that are called
+spl_autoload_register('loadClass');
+
+function loadClass($model) {
+        set_include_path(APPSRC.'models'.DS);
+        spl_autoload(strtolower($model));
+}
+
+// access the coresponding controller for the required page
+$target = APPSRC .'controllers'.DS . $page . '.php';
 
 foreach($parsed_query as $vars) {
-    
+    //split GET vars along '=' symbol to separate variable, values
+    list($variable , $value) = split('=' , $argument);
+    $getVars[$variable] = $value;
 }
 
 if(file_exists($target))
